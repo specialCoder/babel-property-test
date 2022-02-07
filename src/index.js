aa4e930b0714327981fb635409ecbacf8ebccec2
zhubenlei <zhubenlei@zhihu.com>
2022-02-07 13:12:53 +0800

    环境搭建

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+const b = [1,2,3];
+const a = () => {
+    return b.includes(1)
+}
+
+
+export default a;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  target:'web',
+  entry: {
+    app:path.resolve(__dirname,'src','index.js'),
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module:{
+    rules:[
+      {
+        test: /\.(js|ts)x?$/,
+        include:path.resolve(__dirname,'./src'),
+        use: {
+          loader: 'babel-loader',
+        }
+      }],
+  },
+  resolve:{
+    alias:{
+      'src': path.resolve(__dirname,'src'),
+    },
+    mainFiles:['index'],
+    extensions: ['.tsx', '.ts', '.jsx', '.js',  '.json']
+  },
+  plugins:[
+
+  ],
+  output: {
+    filename: 'js/[name].[chunkhash:5].js',
+    chunkFilename: '[name].[chunkhash:8].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+};
